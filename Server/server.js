71e63f208cc6a845c1bda0429f75a484e454ed70
supun-kanda <supunkandmbige9@gmail.com>
2019-03-19 17:22:50 +0000

    adding server files

---
+++ Server/server.js
@@ -0,0 +1,17 @@
+var server = new require("ws").Server;
+var s_local = new server({port:5000});
+
+s_local.on("connection", function(ws){
+    ws.on('message',function(message){
+        message = JSON.parse(message);
+        console.log("Recieved from ", message.name);
+        s_local.clients.forEach(function(element){
+            if(element != ws) element.send(JSON.stringify(message));
+            else element.send(JSON.stringify( {"name": message.name, success:true} ));
+        });
+    });
+    ws.on('close', function(){
+        console.log("I lost a client");
+    })
+});
+console.log("Hello World");
